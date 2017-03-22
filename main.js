bc98c79568f844792de3713ca8d7eec7c89d1542
Miles De Wind <dewinmil@eos13.cis.gvsu.edu>
2017-03-21 21:01:54 -0400

    added in working version for a torus

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 var gl;
 var glCanvas, textOut;
 var orthoProjMat, persProjMat, viewMat, topViewMat, view3Mat, view4mat, ringCF;
-var lightCF, lightPos, pointLight, eyePos;
+var lightCF, lightPos, pointofLight, eyePos;
 var axisBuff, tmpMat, lineBuff;
 var globalAxes;
 var current_view;
@@ -20,8 +20,8 @@ var objTintUnif, ambCoeffUnif, diffCoeffUnif, specCoeffUnif, shininessUnif, isEn
 const IDENTITY = mat4.create();
 var coneSpinAngle;
 var shaderProg;
-var object_hash;
-var obj;
+var object_hash, obj;
+var lightingComponentEnabled = [true, true, true];
 
 const DEFAULT_LIST_TEXT = "Select an object";
 
@@ -31,7 +31,7 @@ function main() {
     gl = WebGLUtils.setupWebGL(glCanvas, null);
     axisBuff = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, axisBuff);
-    let normalCheckBox = document.getElementById("shownormal");
+     let normalCheckBox = document.getElementById("shownormal");
     normalCheckBox.addEventListener('change', ev => {
       showNormal = ev.target.checked;
       redrawNeeded = true;
@@ -41,6 +41,76 @@ function main() {
       showLightVectors = ev.target.checked;
       redrawNeeded = true;
     }, false);
+    let ambientCheckBox = document.getElementById("enableAmbient");
+    ambientCheckBox.addEventListener('change', ev => {
+      lightingComponentEnabled[0] = ev.target.checked;
+      gl.uniform3iv (isEnabledUnif, lightingComponentEnabled);
+      redrawNeeded = true;
+    }, false);
+    let diffuseCheckBox = document.getElementById("enableDiffuse");
+    diffuseCheckBox.addEventListener('change', ev => {
+      lightingComponentEnabled[1] = ev.target.checked;
+      gl.uniform3iv (isEnabledUnif, lightingComponentEnabled);
+      redrawNeeded = true;
+    }, false);
+    let specularCheckBox = document.getElementById("enableSpecular");
+    specularCheckBox.addEventListener('change', ev => {
+      lightingComponentEnabled[2] = ev.target.checked;
+      gl.uniform3iv (isEnabledUnif, lightingComponentEnabled);
+      redrawNeeded = true;
+    }, false);
+    let ambCoeffSlider = document.getElementById("amb-coeff");
+    ambCoeffSlider.addEventListener('input', ev => {
+      gl.uniform1f(ambCoeffUnif, ev.target.value);
+      redrawNeeded = true;
+    }, false);
+    ambCoeffSlider.value = Math.random() * 0.2;
+    let diffCoeffSlider = document.getElementById("diff-coeff");
+    diffCoeffSlider.addEventListener('input', ev => {
+      gl.uniform1f(diffCoeffUnif, ev.target.value);
+      redrawNeeded = true;
+    }, false);
+    diffCoeffSlider.value = 0.5 + 0.5 * Math.random();  // random in [0.5, 1.0]
+    let specCoeffSlider = document.getElementById("spec-coeff");
+    specCoeffSlider.addEventListener('input', ev => {
+      gl.uniform1f(specCoeffUnif, ev.target.value);
+      redrawNeeded = true;
+    }, false);
+    specCoeffSlider.value = Math.random();
+    let shinySlider = document.getElementById("spec-shiny");
+    shinySlider.addEventListener('input', ev => {
+      gl.uniform1f(shininessUnif, ev.target.value);
+      redrawNeeded = true;
+    }, false);
+    shinySlider.value = Math.floor(1 + Math.random() * shinySlider.max);
+    let redSlider = document.getElementById("redslider");
+    let greenSlider = document.getElementById("greenslider");
+    let blueSlider = document.getElementById("blueslider");
+    redSlider.addEventListener('input', colorChanged, false);
+    greenSlider.addEventListener('input', colorChanged, false);
+    blueSlider.addEventListener('input', colorChanged, false);
+ 
+    let objxslider = document.getElementById("objx");
+    let objyslider = document.getElementById("objy");
+    let objzslider = document.getElementById("objz");
+    objxslider.addEventListener('input', objPosChanged, false);
+    objyslider.addEventListener('input', objPosChanged, false);
+    objzslider.addEventListener('input', objPosChanged, false);
+
+    let lightxslider = document.getElementById("lightx");
+    let lightyslider = document.getElementById("lighty");
+    let lightzslider = document.getElementById("lightz");
+    lightxslider.addEventListener('input', lightPosChanged, false);
+    lightyslider.addEventListener('input', lightPosChanged, false);
+    lightzslider.addEventListener('input', lightPosChanged, false);
+
+    let eyexslider = document.getElementById("eyex");
+    let eyeyslider = document.getElementById("eyey");
+    let eyezslider = document.getElementById("eyez");
+    eyexslider.addEventListener('input', eyePosChanged, false);
+    eyeyslider.addEventListener('input', eyePosChanged, false);
+    eyezslider.addEventListener('input', eyePosChanged, false);
+
     window.addEventListener("resize", resizeHandler, false);
     window.addEventListener("keypress", keyboardHandler, false);
     ShaderUtils.loadFromFile(gl, "vshader.glsl", "fshader.glsl")
@@ -54,7 +124,7 @@ function main() {
 	    lineBuff = gl.createBuffer();
 	    posAttr = gl.getAttribLocation(prog, "vertexPos");
 	    colAttr = gl.getAttribLocation(prog, "vertexCol");
-	    normalAttr = gl.getAttribLocation(prog, "jiberish");
+	    normalAttr = gl.getAttribLocation(prog, "vertexNormal");
 	    lightPosUnif = gl.getUniformLocation(prog, "lightPosWorld");
 	    projUnif = gl.getUniformLocation(prog, "projection");
 	    viewUnif = gl.getUniformLocation(prog, "view");
@@ -72,16 +142,19 @@ function main() {
 	    orthoProjMat = mat4.create();
 	    persProjMat = mat4.create();
 	    viewMat = mat4.create();
+	    viewMatInverse = mat4.create();
 	    topViewMat = mat4.create();
 	    view3Mat = mat4.create();
 	    view4Mat = mat4.create();	
 	    ringCF = mat4.create();
 	    lightCF = mat4.create();
 	    tmpMat = mat4.create();
+	    eyePos = vec3.fromValues(3,2,3);
 	    mat4.lookAt(viewMat,
-			vec3.fromValues(5, 5, 5), /* eye */
+			eyePos, /* eye */
 			vec3.fromValues(0, 0, 0), /* focal point */
 			vec3.fromValues(0, 0, 1)); /* up */
+	    mat4.invert(viewMatInverse, viewMat);
 	    mat4.lookAt(topViewMat,
 			vec3.fromValues(0, 0, 2), 
 			vec3.fromValues(0, 0, 0), 
@@ -98,7 +171,13 @@ function main() {
 	    gl.uniformMatrix4fv(modelUnif, false, ringCF);
 	     
 	    lightPos = vec3.fromValues(0, 2, 2);
+	    eyexslider.value = lightPos[0];
+	    eyeyslider.value = lightPos[1];
+	    eyezslider.value = lightPos[2];
 	    mat4.fromTranslation(lightCF, lightPos);		
+	    lightxslider.value = lightPos[0];
+	    lightyslider.value = lightPos[1];
+	    lightzslider.value = lightPos[2];
 	    gl.uniform3fv(lightPosUnif, lightPos);
 	    let vertices = [0, 0, 0, 1, 1, 1,
 		lightPos[0], 0, 0, 1, 1, 1,
@@ -106,13 +185,15 @@ function main() {
 		lightPos[0], lightPos[1], lightPos[2], 1, 1, 1];
 	    gl.bindBuffer(gl.ARRAY_BUFFER, lineBuff);
 	    gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
-	    
-	    objTint = vec3.fromValues(Math.random(), Math.random(), Math.random());
+	    redSlider.value = Math.random();
+	    blueSlider.value = Math.random();
+	    greenSlider.value = Math.random();
+	    objTint = vec3.fromValues(redSlider.value, greenSlider.value, redSlider.value);
 	    gl.uniform3fv(objTintUnif, objTint);
-	    gl.uniform1f(ambCoeffUnif, 1);
-	    gl.uniform1f(diffCoeffUnif, 1);
-	    gl.uniform1f(specCoeffUnif, 1);
-	    gl.uniform1f(shininessUnif, 1);
+	    gl.uniform1f(ambCoeffUnif, ambCoeffSlider.value);
+	    gl.uniform1f(diffCoeffUnif, diffCoeffSlider.value);
+	    gl.uniform1f(specCoeffUnif, specCoeffSlider.value);
+	    gl.uniform1f(shininessUnif, shinySlider.value);
 
 	    //translate everything
 	    var yPos = 0.0;
@@ -124,7 +205,7 @@ function main() {
 	    object_hash["spaceship0"] = new DilbySpaceship(gl, tmpMat);
 	    object_hash["shield0"] = new Planet(gl, 0, 0, 0, 1.0, 75, undefined, 112421442, 1, 4, 0.5, mat4.clone(tmpMat));
 	    let yellow = vec3.fromValues(0xe7/255, 0xf2/255, 0x4d/255);
-	    pointLight = new UniSphere(gl, 0.03, 3, yellow, yellow);
+	    pointofLight = new UniSphere(gl, 0.03, 3, yellow, yellow);
 	    // modelUnif = gl.getUniformLocation(prog, "shield");
 	    addListToView();
 	    //mat4.rotateX(ringCF, ringCF, -Math.PI/2);
@@ -180,6 +261,97 @@ function resizeHandler() {
     } else {
 	alert ("Window is too narrow!");
     }
+}
+
+function ambColorChanged(ev) {
+  switch (ev.target.id) {
+    case 'r-amb-slider':
+      objAmbient[0] = ev.target.value;
+      break;
+    case 'g-amb-slider':
+      objAmbient[1] = ev.target.value;
+      break;
+    case 'b-amb-slider':
+      objAmbient[2] = ev.target.value;
+      break;
+  }
+  gl.uniform3fv(objAmbientUnif, objAmbient);
+  redrawNeeded = true;
+}
+
+function colorChanged(ev) {
+  switch (ev.target.id) {
+    case 'redslider':
+      objTint[0] = ev.target.value;
+      break;
+    case 'greenslider':
+      objTint[1] = ev.target.value;
+      break;
+    case 'blueslider':
+      objTint[2] = ev.target.value;
+      break;
+  }
+  gl.uniform3fv(objTintUnif, objTint);
+  redrawNeeded = true;
+}
+
+function lightPosChanged(ev) {
+  switch (ev.target.id) {
+    case 'lightx':
+      lightPos[0] = ev.target.value;
+      break;
+    case 'lighty':
+      lightPos[1] = ev.target.value;
+      break;
+    case 'lightz':
+      lightPos[2] = ev.target.value;
+      break;
+  }
+  mat4.fromTranslation(lightCF, lightPos);
+  gl.uniform3fv (lightPosUnif, lightPos);
+  let vertices = [
+    0, 0, 0, 1, 1, 1,
+    lightPos[0], 0, 0, 1, 1, 1,
+    lightPos[0], lightPos[1], 0, 1, 1, 1,
+    lightPos[0], lightPos[1], lightPos[2], 1, 1, 1];
+  gl.bindBuffer(gl.ARRAY_BUFFER, lineBuff);
+  gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
+  redrawNeeded = true;
+}
+
+function objPosChanged(ev) {
+  switch (ev.target.id) {
+    case 'objx':
+      ringCF[12] = ev.target.value;
+      break;
+    case 'objy':
+      ringCF[13] = ev.target.value;
+      break;
+    case 'objz':
+      ringCF[14] = ev.target.value;
+      break;
+  }
+  redrawNeeded = true;
+}
+
+function eyePosChanged(ev) {
+  switch (ev.target.id) {
+    case 'eyex':
+      eyePos[0] = ev.target.value;
+      break;
+    case 'eyey':
+      eyePos[1] = ev.target.value;
+      break;
+    case 'eyez':
+      eyePos[2] = ev.target.value;
+      break;
+  }
+  mat4.lookAt(viewMat,
+    eyePos,
+    vec3.fromValues(0, 0, 0), /* focal point */
+    vec3.fromValues(0, 0, 1)); /* up */
+  mat4.invert (viewMatInverse, viewMat);
+  redrawNeeded = true;
 }
 
 function parseSpaceship(objName){
@@ -379,7 +551,7 @@ function drawScene() {
     gl.drawArrays(gl.LINE_STRIP, 0, 4);
 
     // Draw the light source using its own coordinate frame
-    pointLight.draw(posAttr, colAttr, modelUnif, lightCF);
+    pointofLight.draw(posAttr, colAttr, modelUnif, lightCF);
 
     gl.disableVertexAttribArray(colAttr);
     gl.enableVertexAttribArray(normalAttr);
@@ -393,9 +565,7 @@ function draw3D() {
     gl.uniformMatrix4fv(projUnif, false, persProjMat);
     gl.uniformMatrix4fv(viewUnif, false, current_view);
     gl.viewport(0, 0, glCanvas.width, glCanvas.height);
-    obj = new Torus (gl, 1, .3, 26, 24);
-    obj.drawVectorsTo(gl, lightPos, posAttr, colAttr, modelUnif, ringCF);
-    obj.draw(posAttr, normalAttr, modelUnif, ringCF);
+    //obj.drawVectorsTo(gl, lightPos, posAttr, colAttr, modelUnif, ringCF);
     drawScene();
 }
 
